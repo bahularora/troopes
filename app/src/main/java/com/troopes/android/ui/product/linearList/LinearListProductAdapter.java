0fe97f9a4585832d12057fb737fb72ff63f24d79
pcforgeek <pcforgeek@users.noreply.github.com>
2018-09-05 16:02:02 +0530

    removed sample data embedded inside LinearListProductAdapter and refactored it to use Base class

--- app/src/main/java/com/troopes/android/ui/product/linearList/LinearListProductAdapter.java
+++ app/src/main/java/com/troopes/android/ui/product/linearList/LinearListProductAdapter.java
@@ -1,36 +1,19 @@
 package com.troopes.android.ui.product.linearList;
 
 import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.troopes.android.R;
+import com.troopes.android.common.BaseAdapter;
 import com.troopes.android.data.model.Product;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
-public class LinearListProductAdapter extends RecyclerView.Adapter<LinearListProductHolder> {
+public class LinearListProductAdapter extends BaseAdapter<LinearListProductHolder> {
 
-    // TODO - do something and getProduct
     ArrayList<Product> data = new ArrayList<>();
-
-    private ArrayList<Product> getSomeSampleData() {
-        Product one = new Product("lakme", 200, 10101);
-        Product two = new Product("two", 200, 20101);
-        Product three = new Product("HP Ak007tx", 60000, 4642);
-        Product four = new Product("Predator", 80000, 353);
-        Product five = new Product("Thinkpad", 200000, 599);
-        Product six = new Product("iPhone X", 100000, 101);
-        ArrayList<Product> temp = new ArrayList<>(Arrays.asList(one, two, three, four, five, six));
-        return temp;
-    }
-
-    public LinearListProductAdapter() {
-        data = getSomeSampleData();
-    }
 
     @NonNull
     @Override
--- app/src/main/java/com/troopes/android/ui/product/linearList/LinearListProductHolder.java
+++ app/src/main/java/com/troopes/android/ui/product/linearList/LinearListProductHolder.java
@@ -2,7 +2,6 @@ package com.troopes.android.ui.product.linearList;
 
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -11,28 +10,29 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.troopes.android.R;
+import com.troopes.android.common.BaseViewHolder;
 import com.troopes.android.data.model.Product;
 
-public class LinearListProductHolder extends RecyclerView.ViewHolder {
-    private TextView name;
-    private TextView cost;
-    private TextView quantitySold;
-    private ImageView image;
-    private Button gotoProductButton;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LinearListProductHolder extends BaseViewHolder {
+    @BindView(R.id.product_name)
+    TextView name;
+    @BindView(R.id.product_cost)
+    TextView cost;
+    @BindView(R.id.product_quantity_sold)
+    TextView quantitySold;
+    @BindView(R.id.product_image)
+    ImageView image;
+    @BindView(R.id.goto_product_list_button)
+    Button gotoProductButton;
 
     private static final String CURRENCY = "Rs ";
 
     public LinearListProductHolder(@NonNull View itemView) {
         super(itemView);
-        initView(itemView);
-    }
-
-    private void initView(@NonNull View itemView) {
-        name = itemView.findViewById(R.id.product_name);
-        cost = itemView.findViewById(R.id.product_cost);
-        quantitySold = itemView.findViewById(R.id.product_quantity_sold);
-        gotoProductButton = itemView.findViewById(R.id.goto_product_list_button);
-        image = itemView.findViewById(R.id.product_image);
+        ButterKnife.bind(this, itemView);
     }
 
     public void bindData(Product product) {
@@ -47,6 +47,7 @@ public class LinearListProductHolder extends RecyclerView.ViewHolder {
                 Toast.makeText(itemView.getContext(), "Going to product page..", Toast.LENGTH_SHORT).show();
             }
         });
+        // TODO: get product image from net
         Glide.with(itemView.getContext())
                 .load(ContextCompat.getDrawable(itemView.getContext(), R.drawable.sample_product))
                 .into(image);
