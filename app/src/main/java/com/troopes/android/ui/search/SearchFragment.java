5d1fd72fee551771a8c3ceb0c1c1c540974ea085
pcforgeek <pcforgeek@users.noreply.github.com>
2018-09-05 13:22:05 +0530

    added search fragment boilerplate code

---
+++ app/src/main/java/com/troopes/android/ui/search/SearchFragment.java
@@ -0,0 +1,30 @@
+package com.troopes.android.ui.search;
+
+
+import android.view.View;
+
+import com.troopes.android.BaseFragment;
+import com.troopes.android.R;
+
+public class SearchFragment extends BaseFragment {
+
+    public static SearchFragment newInstance() {
+        SearchFragment fragment = new SearchFragment();
+        return fragment;
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.search_fragment;
+    }
+
+    @Override
+    protected int getProgressBarLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void init(View view) {
+        super.init(view);
+    }
+}
