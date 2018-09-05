7731718d1a50d465fabf331d69cfb70b4f161693
pcforgeek <pcforgeek@users.noreply.github.com>
2018-09-05 13:49:14 +0530

    - added productFragment boilerplate code - added string and style resource of product fragment - completed product fragment's layout

---
+++ app/src/main/java/com/troopes/android/ui/product/ProductFragment.java
@@ -0,0 +1,29 @@
+package com.troopes.android.ui.product;
+
+
+import android.view.View;
+
+import com.troopes.android.BaseFragment;
+import com.troopes.android.R;
+
+public class ProductFragment extends BaseFragment {
+
+    public static ProductFragment newInstance(String param1, String param2) {
+        return new ProductFragment();
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.product_fragment;
+    }
+
+    @Override
+    protected int getProgressBarLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void init(View view) {
+        super.init(view);
+    }
+}
