ff49d1803c5c63a1d801e13cdcf92fa484a4d966
pcforgeek <pcforgeek@users.noreply.github.com>
2018-09-11 16:21:39 +0530

    updated product model

--- app/src/main/java/com/troopes/android/data/model/Product.java
+++ app/src/main/java/com/troopes/android/data/model/Product.java
@@ -1,18 +1,19 @@
 package com.troopes.android.data.model;
 
 public class Product {
-    public Product(String name, int cost, long itemsSold) {
+    public long productId;
+
+    public Product(long productId, String name, int cost, long itemsSold, String imageUrl) {
         this.name = name;
         this.cost = cost;
         this.itemsSold = itemsSold;
+        this.imageUrl = imageUrl;
+        this.productId = productId;
     }
-
     public String name;
     // don't want product to cost money in denomination smaller than 1 rs
     public int cost;
     public long itemsSold;
-
-    // TODO
     public String productUrl = null;
     public String imageUrl = null;
 }
