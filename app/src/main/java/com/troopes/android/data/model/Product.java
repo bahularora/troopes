d18d20ed5eaa23369c00e930e0ba37f23072f686
pcforgeek <pcforgeek@users.noreply.github.com>
2018-09-11 16:41:02 +0530

    updated product model

--- app/src/main/java/com/troopes/android/data/model/Product.java
+++ app/src/main/java/com/troopes/android/data/model/Product.java
@@ -1,19 +1,32 @@
 package com.troopes.android.data.model;
 
+import com.troopes.android.data.model.product.Color;
+import com.troopes.android.data.model.product.Size;
+import com.troopes.android.data.model.product.Variant;
+
+import java.util.ArrayList;
+
 public class Product {
     public long productId;
 
-    public Product(long productId, String name, int cost, long itemsSold, String imageUrl) {
-        this.name = name;
-        this.cost = cost;
-        this.itemsSold = itemsSold;
-        this.imageUrl = imageUrl;
-        this.productId = productId;
-    }
+    public ArrayList<Color> colorList;
     public String name;
     // don't want product to cost money in denomination smaller than 1 rs
     public int cost;
     public long itemsSold;
     public String productUrl = null;
     public String imageUrl = null;
+    public ArrayList<Size> sizeList;
+    public ArrayList<Variant> variantList;
+
+    public Product(long productId, String name, int cost, long itemsSold, String imageUrl, ArrayList<Color> colorList, ArrayList<Size> sizeList, ArrayList<Variant> variantList) {
+        this.name = name;
+        this.cost = cost;
+        this.itemsSold = itemsSold;
+        this.imageUrl = imageUrl;
+        this.productId = productId;
+        this.colorList = colorList;
+        this.sizeList = sizeList;
+        this.variantList = variantList;
+    }
 }
