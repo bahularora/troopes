add9c86b5a0e49ba84da9d8021d4ff100071b6f7
pcforgeek <pcforgeek@users.noreply.github.com>
2018-09-12 15:55:04 +0530

    added search section item (layout, holder and adapter)

---
+++ app/src/main/java/com/troopes/android/ui/search/SectionAdapter.java
@@ -0,0 +1,39 @@
+package com.troopes.android.ui.search;
+
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.troopes.android.R;
+import com.troopes.android.common.BaseAdapter;
+import com.troopes.android.data.model.Search;
+
+import java.util.ArrayList;
+
+public class SectionAdapter extends BaseAdapter<SectionHolder> {
+
+    private ArrayList<Search> data = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public SectionHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.search_section_item, viewGroup, false);
+        return new SectionHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SectionHolder sectionHolder, int position) {
+        sectionHolder.bindData(data.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public void setData(ArrayList<Search> data) {
+        this.data = data;
+        notifyDataSetChanged();
+    }
+}
---
+++ app/src/main/java/com/troopes/android/ui/search/SectionHolder.java
@@ -0,0 +1,49 @@
+package com.troopes.android.ui.search;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.TextView;
+
+import com.troopes.android.R;
+import com.troopes.android.common.BaseViewHolder;
+import com.troopes.android.data.model.Search;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SectionHolder extends BaseViewHolder {
+
+    @BindView(R.id.section_title)
+    TextView title;
+    @BindView(R.id.see_more)
+    TextView seeMore;
+    @BindView(R.id.search_section_item_list)
+    RecyclerView sectionItemList;
+
+    public SectionHolder(@NonNull View itemView) {
+        super(itemView);
+        ButterKnife.bind(this, itemView);
+    }
+
+    private int calculateNoOfColumns(Context context) {
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
+        // here 72+4+4(margin) is the dp width of each viewholder
+        return (int) (dpWidth / 80);
+    }
+
+    public void bindData(Search item) {
+        title.setText(item.name);
+
+        int subCategoryListGridCount = calculateNoOfColumns(itemView.getContext());
+        GridLayoutManager glm = new GridLayoutManager(itemView.getContext(), subCategoryListGridCount);
+        SectionInnerAdapter adapter = new SectionInnerAdapter();
+        adapter.setData(item.list);
+        sectionItemList.setLayoutManager(glm);
+        sectionItemList.setAdapter(adapter);
+    }
+}
