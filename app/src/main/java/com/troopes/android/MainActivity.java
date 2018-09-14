9447b6d1fccc7d4b7bf8597eab937a31a10efd16
pcforgeek <pcforgeek@users.noreply.github.com>
2018-09-14 15:13:46 +0530

    refactored main activity

--- app/src/main/java/com/troopes/android/MainActivity.java
+++ app/src/main/java/com/troopes/android/MainActivity.java
@@ -6,7 +6,9 @@ import android.support.design.widget.BottomNavigationView;
 import android.view.MenuItem;
 
 import com.troopes.android.common.BaseActivity;
+import com.troopes.android.ui.account.AccountFragment;
 import com.troopes.android.ui.home.HomeFragment;
+import com.troopes.android.ui.search.SearchFragment;
 import com.troopes.android.viewmodel.MainViewModel;
 
 import butterknife.BindView;
@@ -20,13 +22,12 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected int getLayoutResId() {
-        return R.layout.activity_home;
+        return R.layout.activity_main;
     }
 
     @Override
     protected void init() {
         super.init();
-        hideToolbar();
 
         mainViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
 
@@ -36,13 +37,13 @@ public class MainActivity extends BaseActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.home:
-                        replaceFragment(new HomeFragment());
+                        replaceFragment(HomeFragment.newInstance());
                         break;
                     case R.id.search:
-                        replaceFragment(new HomeFragment());
+                        replaceFragment(SearchFragment.newInstance(false));
                         break;
                     case R.id.account:
-                        replaceFragment(new HomeFragment());
+                        replaceFragment(AccountFragment.newInstance());
                         break;
                 }
                 return true;
