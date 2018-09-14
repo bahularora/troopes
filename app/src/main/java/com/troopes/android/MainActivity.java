b92a40e80b6ed7a7b8fda60f1bf1d0a9912a923f
pcforgeek <pcforgeek@users.noreply.github.com>
2018-09-14 15:24:59 +0530

    added toolbar to main activity

--- app/src/main/java/com/troopes/android/MainActivity.java
+++ app/src/main/java/com/troopes/android/MainActivity.java
@@ -3,12 +3,14 @@ package com.troopes.android;
 import android.arch.lifecycle.ViewModelProviders;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.view.MenuItem;
 
 import com.troopes.android.common.BaseActivity;
 import com.troopes.android.ui.account.AccountFragment;
 import com.troopes.android.ui.home.HomeFragment;
 import com.troopes.android.ui.search.SearchFragment;
+import com.troopes.android.utils.ToolbarUtils;
 import com.troopes.android.viewmodel.MainViewModel;
 
 import butterknife.BindView;
@@ -23,6 +25,27 @@ public class MainActivity extends BaseActivity {
     @Override
     protected int getLayoutResId() {
         return R.layout.activity_main;
+    }
+
+    @Override
+    protected void setToolbar(Fragment fragment) {
+        if (fragment.getClass().getSimpleName().equals(HomeFragment.class.getSimpleName())) {
+            ToolbarUtils.hide(MainActivity.this);
+        } else if (fragment.getClass().getSimpleName().equals(SearchFragment.class.getSimpleName())) {
+            ToolbarUtils.show(MainActivity.this);
+            ToolbarUtils.hideToolbar(MainActivity.this);
+            ToolbarUtils.showSearchBar(MainActivity.this);
+        } else if (fragment.getClass().getSimpleName().equals(AccountFragment.class.getSimpleName())) {
+            ToolbarUtils.show(MainActivity.this);
+            ToolbarUtils.hideSearchBar(MainActivity.this);
+            ToolbarUtils.showToolbar(MainActivity.this);
+            ToolbarUtils.hideShare(MainActivity.this);
+            ToolbarUtils.hideBackButton(MainActivity.this);
+            ToolbarUtils.hideFavourite(MainActivity.this);
+            ToolbarUtils.setToolbarTitle(MainActivity.this, "Accounts");
+        } else {
+            ToolbarUtils.hide(this);
+        }
     }
 
     @Override
