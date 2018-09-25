7afc6f6a30a86e63f982582557833c9d16d1c161
pcforgeek <pcforgeek@users.noreply.github.com>
2018-09-25 13:40:28 +0530

    added Tag model

---
+++ app/src/main/java/com/troopes/android/data/model/Tag.java
@@ -0,0 +1,11 @@
+package com.troopes.android.data.model;
+
+public class Tag {
+    public String name;
+    public String url;
+
+    public Tag(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+}
