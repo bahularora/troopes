c06ad8675024d3ad299526fddeac66312b4d745e
pcforgeek <pcforgeek@users.noreply.github.com>
2018-10-04 18:05:19 +0530

    completed OrderPagerAdapter

---
+++ app/src/main/java/com/troopes/android/ui/account/OrderPagerAdapter.java
@@ -0,0 +1,32 @@
+package com.troopes.android.ui.account;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class OrderPagerAdapter extends FragmentPagerAdapter {
+
+    public OrderPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        String type = "";
+        if (position == 0) {
+            type = "all";
+        } else if (position == 1) {
+            type = "pending";
+        } else if (position == 2) {
+            type = "delivered";
+        } else if (position == 3) {
+            type = "cancelled";
+        }
+        return OrderTypeFragment.newInstance(type);
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
