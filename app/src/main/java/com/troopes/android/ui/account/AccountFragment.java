0d0cc7c09962aaacb78d6dc02103f53edc2d95b7
pcforgeek <pcforgeek@users.noreply.github.com>
2018-10-08 14:12:00 +0530

    changes to AccountFragment to open other fragment on item click

--- app/src/main/java/com/troopes/android/ui/account/AccountFragment.java
+++ app/src/main/java/com/troopes/android/ui/account/AccountFragment.java
@@ -1,12 +1,40 @@
 package com.troopes.android.ui.account;
 
 
+import android.support.constraint.ConstraintLayout;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.troopes.android.R;
 import com.troopes.android.common.BaseFragment;
+import com.troopes.android.ui.account.myOrders.MyOrdersFragment;
+
+import butterknife.BindView;
 
 public class AccountFragment extends BaseFragment {
+
+    @BindView(R.id.profile_image)
+    ImageView profileImage;
+    @BindView(R.id.profile_name_welome)
+    TextView profileName;
+    @BindView(R.id.about_container)
+    ConstraintLayout aboutContainer;
+    @BindView(R.id.faq_container)
+    ConstraintLayout faqContainer;
+    @BindView(R.id.policy_container)
+    ConstraintLayout policyContainer;
+    @BindView(R.id.my_orders_container)
+    ConstraintLayout myOrdersContainer;
+    @BindView(R.id.wishlist_container)
+    ConstraintLayout wishlistContainer;
+    @BindView(R.id.accounts_container)
+    ConstraintLayout accountsContainer;
+    @BindView(R.id.my_profile_container)
+    ConstraintLayout myProfileContainer;
+
+    OnFragmentInteractionListener onFragmentInteractionListener;
+
     public static AccountFragment newInstance() {
         return new AccountFragment();
     }
@@ -24,5 +52,54 @@ public class AccountFragment extends BaseFragment {
     @Override
     protected void init(View view) {
         super.init(view);
+        aboutContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // do something
+            }
+        });
+        faqContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // do something
+            }
+        });
+        policyContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // do something
+            }
+        });
+        myOrdersContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyOrdersFragment fragment = MyOrdersFragment.newInstance();
+                onFragmentInteractionListener.onInteraction(fragment);
+            }
+        });
+        wishlistContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // do something
+            }
+        });
+        accountsContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // do something
+            }
+        });
+        myProfileContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyProfileFragment fragment = MyProfileFragment.newInstance();
+                onFragmentInteractionListener.onInteraction(fragment);
+            }
+        });
+    }
+
+    @Override
+    public void setOnFragmentInteractionListener(OnFragmentInteractionListener onFragmentInteractionListener) {
+        this.onFragmentInteractionListener = onFragmentInteractionListener;
     }
 }
